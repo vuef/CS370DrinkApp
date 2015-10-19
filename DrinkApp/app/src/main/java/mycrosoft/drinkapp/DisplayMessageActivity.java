d6e5d58eed03ce569b5892837e9b6a4d5362e846
MatthewBrian Sweeney <msweeney@cwolf.cs.sonoma.edu>
2015-10-19 00:44:03 -0700

    Possible foundation for the app

---
+++ DrinkApp/app/src/main/java/mycrosoft/drinkapp/DisplayMessageActivity.java
@@ -0,0 +1,52 @@
+package mycrosoft.drinkapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+       /*
+       Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        */
+
+        //Get the message from intent
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(Main.EXTRA_MESSAGE);
+
+        //Create the text view
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        //Set the text view as the activity layout
+        setContentView(textView);
+
+    }
+
+
+
+
+}
