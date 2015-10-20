c65a769bf5ac8f9a89ca42f4e6ee4ea0a1d239ce
fern_vue@hotmail.com <fvue@cwolf.cs.sonoma.edu>
2015-10-20 00:08:35 -0700

    Scraper protoype 1

--- DrinkApp/app/src/main/java/mycrosoft/drinkapp/Main.java
+++ DrinkApp/app/src/main/java/mycrosoft/drinkapp/Main.java
@@ -6,11 +6,9 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
-
 
 
 public class Main extends AppCompatActivity {
@@ -57,4 +55,5 @@ public class Main extends AppCompatActivity {
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
     }
+
 }
