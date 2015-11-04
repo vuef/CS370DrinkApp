9b88e146908f698bac8eee48cbefa41a81480fd0
MatthewBrian Sweeney <msweeney@cwolf.cs.sonoma.edu>
2015-11-04 12:13:51 -0800

    base for rest of the project

--- DrinkApp/app/src/main/java/mycrosoft/drinkapp/DisplayMessageActivity.java
+++ DrinkApp/app/src/main/java/mycrosoft/drinkapp/DisplayMessageActivity.java
@@ -16,21 +16,7 @@ public class DisplayMessageActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
-       /*
-       Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        */
 
         //Get the message from intent
         Intent intent = getIntent();
--- DrinkApp/app/src/main/java/mycrosoft/drinkapp/Main.java
+++ DrinkApp/app/src/main/java/mycrosoft/drinkapp/Main.java
@@ -9,8 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
-import Activity.DisplayMessageActivity;
-import Activity.WhatCanIMakeActivity;
+
 
 
 public class Main extends AppCompatActivity {
@@ -22,14 +21,6 @@ public class Main extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
